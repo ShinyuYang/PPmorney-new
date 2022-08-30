cbd3d75fb5fcc4c2f514a8ccde51f02b7d372ea2
ShinyuYang <1449404535@qq.com>
2022-08-30 21:00:55 +0800

    封装window.createTag

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,16 @@ Vue.component('Icon',Icon)
 
 window.tagList=tagListModel.fetch();
 
+window.createTag = (name: string)=>{
+    const  message=tagListModel.create(name);
+    if(message==='duplicated'){
+      window.alert('标签重复');
+    }else if(message==='success'){
+      window.alert('添加成功');
+    }
+  };
+
+
 new Vue({
   router,
   store,
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -28,15 +28,10 @@ tagListModel.fetch();
 export default class Labels extends Vue {
   tags=window.tagList;
 
-  createTag(){
-    const name=window.prompt('请输入标签名');
+  createTag() {
+    const name = window.prompt('请输入标签名');
     if(name){
-      const  message=tagListModel.create(name);
-      if(message==='duplicated'){
-        window.alert('标签重复');
-      }else if(message==='success'){
-        window.alert('添加成功');
-      }
+      window.createTag(name);
     }
   }
 }
