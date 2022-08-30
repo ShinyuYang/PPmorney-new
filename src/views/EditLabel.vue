50c8c8c59fda073d6cb2e82cae87d9cd0c0c3dc3
ShinyuYang <1449404535@qq.com>
2022-08-30 10:03:34 +0800

    展示tag的name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
     <span class="rightIcon"></span>
   </div>
   <div class="form-wrapper">
-  <Notes field-name="标签名" placeholder="请输入标签名"/>
+  <Notes :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
   </div>
   <div class="button-wrapper">
   <Button>删除标签</Button>
@@ -25,13 +25,15 @@ import Button from '@/components/Button.vue';
   components: {Button, Notes}
 })
 export default class EditLabel extends Vue {
+  tag?: {id: string;name: string}=undefined;
+  // 再通过created赋值
  created(){
    const id = this.$route.params.id;
    tagListModel.fetch();
    const tags=tagListModel.data;
    const tag =tags.filter(t =>t.id===id)[0];
    if(tag){
-     console.log(tag);
+     this.tag=tag;
    }else{
      this.$router.replace('/404');
    }//实现点击一个标签出现对应标签的编辑页面,跟route页面关联
